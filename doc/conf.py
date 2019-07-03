5744b8c1bc13dd015a4ce69a132bd44e1ff59f54
huangyh09 <huangyh094@gmail.com>
2019-07-03 22:53:15 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ import sphinx_bootstrap_theme
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
